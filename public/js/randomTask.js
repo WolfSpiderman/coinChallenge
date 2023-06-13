a188786a6a393de4cf6f7c945c769cd4afb5200c
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-13 14:21:21 -0500

    fixed script and image src's in handlebars

--- public/js/randomTask.js
+++ public/js/randomTask.js
@@ -3,18 +3,18 @@ const tasks = [
     task: 'Start a "Cult! Cult! Cult!" Chant!',
     description:
       'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
-    image: '../images/Cult.webp',
+    image: '/images/Cult.webp',
   },
   {
     task: 'Giant Stone Balls!',
     description: 'Do your best David Hatcher Childress impression!',
-    image: '../images/GiantStoneBalls.webp',
+    image: '/images/GiantStoneBalls.webp',
   },
   {
     task: 'Air Banjo!',
     description:
       'Play something on the air banjo until someone is able to identify the song! It\'s Air Banjo Charades*!   *Kinda',
-    image: '../images/AirBanjo.webp',
+    image: '/images/AirBanjo.webp',
   },
   {
     task: 'Top 5 (or Top 3) Takeaways!',
