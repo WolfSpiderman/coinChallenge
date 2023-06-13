df983756708a828ead7ad4e862ec57a3c67ae418
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-13 14:05:20 -0500

    added params to homeRoutes routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 
-router.get('/', res.render('randomTask'));
+router.get('/', (req, res) => res.render('randomTask'));
 
-router.get('/suggestions', res.render('suggest'));
+router.get('/suggestions', (req, res) => res.render('suggest'));
