d446d84f7630c152f445ff4a1a6888d91540a5ba
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-15 03:09:03 -0500

    added delete route

--- controllers/api/suggestController.js
+++ controllers/api/suggestController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { where } = require('sequelize');
 const { Suggest } = require('../../models');
 
 router.get('/whipple', async (req, res) => {
@@ -18,5 +19,20 @@ router.post('/whipple', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.delete('/whipple/steveRogersCanWieldMjolnir/:id/', async (req, res) => {
+    try {
+        const suggestData = await Suggest.destroy({
+            where: { id: req.params.id }
+    });
+    if (!suggestData) {
+        res.status(404).json({ message: 'No suggestion with that ID exists, shit-for-brains.'});
+        return;
+    }
+    res.status(200).json({ message: `Successfully deleted: ${suggestData}!` });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
