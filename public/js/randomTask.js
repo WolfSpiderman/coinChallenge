deea5f3081d8a039c4a5e6bb97b4181d3a4f2fe2
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-14 23:39:11 -0500

    updated css and added some random tasks

--- public/js/randomTask.js
+++ public/js/randomTask.js
@@ -20,6 +20,16 @@ const tasks = [
     task: 'Top 5 (or Top 3) Takeaways!',
     description: 'List your top 5 episodes! Or top 3 serial killer episodes, top 3 cult episodes, top 3 STDP stories, top 3 Behind The Sucks, top 3 Timesuck genres, etc.',
     image: '/images/top5.jpg'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'Secret Suck Challenge!',
+    description: 'Pick the first Secret Suck challenge that comes to the top of your head that you can do or have a story for and do it! If you don\'t happen to be a Space Lizard and don\'t know what this is referring to, just hit the button again.',
+    image: '/images/SpaceLizard.png'
   }
 ];
 
