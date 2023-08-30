347c5a34473a86b205a631e589ac3e4a5ab02de7
WolfSpiderman <wolfspiderman@yahoo.com>
2023-08-30 17:15:04 -0500

    updated frequency of some tasks, added WYR

--- public/js/randomTask.js
+++ public/js/randomTask.js
@@ -6,6 +6,76 @@ const tasks = [
     image: '/images/Cult.webp',
   },
   {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Start a "Cult! Cult! Cult!" Chant!',
+    description:
+      'Start a wave of cult chants! Three rounds of the good ol\' "Cult! Cult! Cult" as loud as you can to get as many people around you as possible doing it too! Maybe practice modesty if you happen to be indoors, however.',
+    image: '/images/Cult.webp',
+  },
+  {
+    task: 'Giant Stone Balls!',
+    description: 'Do your best David Hatcher Childress impression!',
+    image: '/images/GiantStoneBalls.webp',
+  },
+  {
+    task: 'Air Banjo!',
+    description:
+      'Play something on the air banjo until someone is able to identify the song! It\'s Air Banjo Charades*!   *Kinda',
+    image: '/images/AirBanjo.webp',
+  },
+  {
+    task: 'Top 5 (or Top 3) Takeaways!',
+    description: 'List your top 5 episodes! Or top 3 serial killer episodes, top 3 cult episodes, top 3 STDP stories, top 3 Behind The Sucks, top 3 Timesuck genres, etc.',
+    image: '/images/top5.jpg'
+  },
+  {
     task: 'Giant Stone Balls!',
     description: 'Do your best David Hatcher Childress impression!',
     image: '/images/GiantStoneBalls.webp',
@@ -27,9 +97,59 @@ const tasks = [
     image: '/images/Nimrod.png'
   },
   {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
+    task: 'HAIL NIMROD!',
+    description: 'Shout "HAIL NIMROD!" at the top of your breath! Take a second to prepare your throat first, and maybe also to give those in immediate earshot a polite heads-up, and then let \'er rip!',
+    image: '/images/Nimrod.png'
+  },
+  {
     task: 'Secret Suck Challenge!',
     description: 'Pick the first Secret Suck challenge that comes to the top of your head that you can do or have a story for and do it! If you don\'t happen to be a Space Lizard and don\'t know what this is referring to, just hit the button again.',
     image: '/images/SpaceLizard.png'
+  },
+  {
+    task: 'Would You Rather!',
+    description: 'Think up a good "Would You Rather" question for your group to discuss.',
+    image: '/images/DanCumminsThinker.jpg'
   }
 ];
 
